9be048b463d2f84f2d768184a2d3587953e500be
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-09-29 23:11:43 +0300

    make api path public

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextRequest } from 'next/server';
 
-// إنشاء matcher للمسارات العامة
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/:path*']);
 
 export default clerkMiddleware((auth, req: NextRequest) => {
-  // التحقق مما إذا كان المسار هو مسار عام باستخدام NextRequest
+  console.log("Checking route:", req.nextUrl.pathname);
+
   if (isPublicRoute(req)) {
-    return; // لا حاجة لتطبيق حماية المصادقة على المسارات العامة
+    console.log("Public route detected, skipping authentication:", req.nextUrl.pathname);
+    return;
   }
 
-  // إذا كان المسار غير عام، يتم فرض المصادقة
+  console.log("Protected route, enforcing authentication:", req.nextUrl.pathname);
   auth().protect();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
