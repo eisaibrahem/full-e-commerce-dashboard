f61f6de34f53698345309219aa5613ebdefc4a7b
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-10-01 23:05:00 +0300

    test

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextRequest } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/:path*', '/webhook']);
-
-export default clerkMiddleware((auth, req: NextRequest) => {
-  console.log("Checking route:", req.nextUrl.pathname);
-
-  if (isPublicRoute(req)) {
-    console.log("Public route detected, skipping authentication:", req.nextUrl.pathname);
-    return;
-  }
-
-  // مؤقتًا قم بإلغاء enforce المصادقة لاختبار الوصول بدون Clerk
-   auth().protect();
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
+    // Add public routes here
+    '/api/:path*',  // إضافة المسار العام هنا
   ],
-};
+}
