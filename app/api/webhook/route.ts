91d9c3bf3946deadf1fecd1b3473464e8d058936
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-10-03 15:52:01 +0300

    solve bug

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,28 +1,28 @@
 import Stripe from "stripe";
-import { headers } from "next/headers"
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
-  const body = await req.text()
-  const signature = headers().get("Stripe-Signature") as string
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 
-  let event: Stripe.Event
+  let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
-    ) 
+    );
   } catch (error) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
   }
 
-  const session = event.data.object as Stripe.Checkout.Session
-  const address = session?.customer_details?.address
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
 
   const addressComponents = [
     address?.line1,
@@ -31,38 +31,46 @@ export async function POST(req: Request) {
     address?.state,
     address?.postal_code,
     address?.country,
-  ]
+  ];
 
-  const addressString = addressComponents.filter((c) => c !== null).join(', ')
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
 
   if (event.type === "checkout.session.completed") {
-    const order = await prismadb.order.update({
+    const existingOrder = await prismadb.order.findUnique({
       where: {
-        id: session?.metadata?.orderId
+        id: session?.metadata?.orderId,
       },
-      data: {
-        isPaid: true,
-        address: addressString,
-        phone: session?.customer_details?.phone || ''
-      },
-      include: {
-        orderItems: true
-      }
-    })
+    });
 
-    const productIds = order.orderItems.map((orderItem) => orderItem.productId)
+    if (!existingOrder || !existingOrder.isPaid) {
+      const order = await prismadb.order.update({
+        where: {
+          id: session?.metadata?.orderId,
+        },
+        data: {
+          isPaid: true,
+          address: addressString,
+          phone: session?.customer_details?.phone || "",
+        },
+        include: {
+          orderItems: true,
+        },
+      });
 
-    await prismadb.product.updateMany({
-      where: {
-        id: {
-          in: [...productIds]
-        }
-      },
-      data: {
-        isArchived: true
-      }
-    })
+      const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+      await prismadb.product.updateMany({
+        where: {
+          id: {
+            in: [...productIds],
+          },
+        },
+        data: {
+          isArchived: true,
+        },
+      });
+    }
   }
 
-  return new NextResponse(null, { status: 200 })
+  return new NextResponse(null, { status: 200 });
 }
